f170896f2165febe90030fe2e100161b8425d2aa
Shehfinaz Kadavil <shehfinaz@gmail.com>
2020-06-16 08:42:37 +0200

    First commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        //return new Response('<h1>Hi</h1>');
+        return $this->render('/home/index.html.twig');
+    }
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        dump($request->get('name'));
+        $name = $request->get('name');
+        //return new Response('<h1>Hi this is custom '.$request->get('name').'</h1>');
+        return $this->render('/home/custom.html.twig',[
+            'name' => $name,
+        ]);
+    }
+}
+
---
+++ src/Controller/PostController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+
+/**
+ * @Route("/post", name="post.")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param PostRepository
+     * @return Response
+     */
+    public function index(PostRepository $postRepository)
+    {
+        $posts = $postRepository->findAll();
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return Response
+     */
+    public function create(Request $request)
+    {
+      $post  = new Post();
+      $form = $this->createForm(PostType::class,$post);
+      $form->handleRequest($request);
+      if($form->isSubmitted()) {
+          $em = $this->getDoctrine()->getManager();
+         // dump($request->files); shows the content of the form
+          /**@var UploadedFile $file */
+
+          $file = $request->files->get('post')['attachment']; //same name 'attachment' given in form child
+          if($file) {
+              $filename = md5(uniqid()).'.'.$file->guessClientExtension();
+              $file->move(
+                  $this->getParameter('uploads_dir'),
+                  $filename
+              );
+              $post->setImage($filename);
+          }
+          $em->persist($post);
+          $em->flush();
+          return $this->redirect($this->generateUrl('post.index'));
+      }
+
+      //$post->setTitle('This is a post');
+      //$em = $this->getDoctrine()->getManager();
+      //$em->persist($post);
+      //$em->flush();
+      //return ($this->redirect($this->generateUrl('post.index')));
+      return $this->render('post/create.html.twig',['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param PostRepository $postRepository
+     * @param $id
+     * @return Response
+    */
+    public function show($id,PostRepository $postRepository)
+    {
+        $post = $postRepository->find($id);
+        return $this->render('post/show.html.twig',['post' => $post]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param PostRepository $postRepository
+     * @param $id
+     * @return Response
+
+    public function show($id,PostRepository $postRepository)
+    {
+        $post = $postRepository->findPostWithCategory($id);
+        dump($post);
+        die;
+
+        return $this->render('post/show.html.twig',['post' => $post]);
+    } */
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * Using paramconverter
+     * @param Post $post
+     * @return Response
+
+   public function show(Post $post)
+   {
+       return $this->render('post/show.html.twig',['post' => $post]);
+   }*/
+
+    /**
+     * @Route("/remove/{id}", name="remove")
+     * @param Post $post
+     * Using paramconverter
+
+    public function remove(Post $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+     * return ($this->redirect($this->generateUrl('post.index')));
+    }*/
+
+    /**
+     * @Route("/remove/{id}", name="remove")
+     * @param PostRepository $postRepository
+     * @return RedirectResponse
+     */
+    public function remove($id,PostRepository $postRepository)
+    {
+        $post = $postRepository->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+        $this->addFlash('success','Your post has been removed');
+        return ($this->redirect($this->generateUrl('post.index')));
+    }
+
+}
